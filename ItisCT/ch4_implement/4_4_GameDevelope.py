b2a9044ce202323818fa950c7ad4dcac8f7dd1cf
yhj0214 <87259492+yhj0214@users.noreply.github.com>
2022-09-23 20:18:31 +0900

    4_4 게임개발문제 끝

--- ItisCT/ch4_implement/4_4_GameDevelope.py
+++ ItisCT/ch4_implement/4_4_GameDevelope.py
@@ -1,3 +1,4 @@
+from array import array
 from glob import glob
 
 
@@ -19,3 +20,35 @@ def turn_left():
     direction = direction - 1
     if direction == -1:
         direction =3 
+
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if answerarr[nx][ny] == 0 and mapinfo[nx][ny] == 0:
+        answerarr[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    else:
+        turn_time += 1
+
+    if turn_time == 4:
+        nx = x-dx[direction]
+        ny = y-dy[direction]
+
+        if mapinfo[nx][ny] == 0:
+            x = nx
+            y = ny
+
+        else:
+            break
+        turn_time = 0
+
+        
+print(count)
