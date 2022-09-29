eb9e40f1fb70392ef3ab620eeffb254e5af65113
yhj0214 <87259492+yhj0214@users.noreply.github.com>
2022-09-29 16:05:20 +0900

    .

--- ItisCT/ch6_Sort/6_2_swap.py
+++ ItisCT/ch6_Sort/6_2_swap.py
@@ -1,8 +1,3 @@
-# array = [3, 5]
-# array[0], array[1] = array[1], array[0]
-# print(array)
-
-a = 3
-b = 5
-a, b = b, a
-print(a, b)
+array = [3, 5]
+array[0], array[1] = array[1], array[0]
+print(array)
